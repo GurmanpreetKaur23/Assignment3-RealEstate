5cb76a0dba8b619a3327c85170d46a9d58ecb5b0
gurman <gurmanpreet341.be22@chitkara.edu.in>
2024-04-19 14:52:39 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import "./App.css"
-import Pages from "./components/pages/Pages"
+import React, {useState} from 'react'
+import Dropdown from './components/Dropdown';
+import Hero from './components/Hero';
+import InfoSection from './components/InfoSection';
+import Navbar from './components/Navbar';
+import { InfoData } from './data/InfoData';
+import { SliderData } from './data/SliderData';
+import GlobalStyle from './globalStyles';
 
 function App() {
-  return <Pages />
+  const[isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  };
+
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Hero slides={ SliderData }/>
+      <InfoSection {...InfoData}/>
+    </>
+  );
 }
 
-export default App
+export default App;
